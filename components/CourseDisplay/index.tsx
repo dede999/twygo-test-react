62c0e522cc19e1a8d94401c181af4e5b3d901ee9
Andre Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2024-09-16 15:35:43 -0300

    Change a menu for a drawer
    I'm making this as it looks much more interesting to have a drawer than
    some menus and modals

--- components/CourseDisplay/index.tsx
+++ components/CourseDisplay/index.tsx
@@ -4,58 +4,79 @@ import {
   CardBody,
   CardFooter,
   CardHeader,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
   Heading,
   Stack,
   Text,
+  useDisclosure,
 } from "@chakra-ui/react";
 import { Course } from "../../src/mockAPI/types";
-import CourseMenu from "../CourseMenu";
+// import CourseMenu from "../CourseMenu";
 import { faCalendarXmark } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCirclePlay } from "@fortawesome/free-solid-svg-icons/faCirclePlay";
+import { faEye } from "@fortawesome/free-solid-svg-icons/faEye";
+import CourseDrawer from "../CourseDrawer";
 
 export default function CourseDisplsy({ course }: { course: Course }) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-    <Card
-      p={4}
-      borderWidth={1}
-      borderBottomWidth={3}
-      borderRadius={24}
-      overflow="hidden"
-      className="course-card"
-      borderColor="purple.400"
-    >
-      <CardHeader>
-        <Heading as="h3" size="md">
-          {course.title}
-        </Heading>
-      </CardHeader>
-      <CardBody>
-        <Text>{course.description}</Text>
-      </CardBody>
-      <CardFooter>
-        <Stack direction="row">
-          <Button
-            variant="outline"
-            disabled
-            width="fit-content"
-            colorScheme="purple"
-            leftIcon={<FontAwesomeIcon size="xl" icon={faCalendarXmark} />}
-          >
-            {course.expirationDate.toLocaleDateString()}
-          </Button>
-          <Button
-            variant="outline"
-            disabled
-            width="fit-content"
-            colorScheme="purple"
-            leftIcon={<FontAwesomeIcon size="xl" icon={faCirclePlay} />}
-          >
-            {course.courseVideos.length}
-          </Button>
-          <CourseMenu />
-        </Stack>
-      </CardFooter>
-    </Card>
+    <>
+      <Card
+        borderWidth={1}
+        borderBottomWidth={3}
+        borderRadius={24}
+        overflow="hidden"
+        className="course-card"
+        borderColor="purple.400"
+      >
+        <CardHeader>
+          <Heading as="h3" size="md">
+            {course.title}
+          </Heading>
+        </CardHeader>
+        <CardBody>
+          <Text>{course.description}</Text>
+        </CardBody>
+        <CardFooter>
+          <Stack direction="row">
+            <Button
+              variant="outline"
+              disabled
+              width="fit-content"
+              colorScheme="purple"
+              leftIcon={<FontAwesomeIcon size="xl" icon={faCalendarXmark} />}
+            >
+              {course.expirationDate.toLocaleDateString()}
+            </Button>
+            <Button
+              variant="outline"
+              disabled
+              width="fit-content"
+              colorScheme="purple"
+              leftIcon={<FontAwesomeIcon size="xl" icon={faCirclePlay} />}
+            >
+              {course.courseVideos.length}
+            </Button>
+            <Button
+              variant="solid"
+              disabled
+              width="fit-content"
+              colorScheme="purple"
+              onClick={onOpen}
+              leftIcon={<FontAwesomeIcon size="xl" icon={faEye} />}
+            />
+          </Stack>
+        </CardFooter>
+      </Card>
+      <CourseDrawer isOpen={isOpen} course={course} onClose={onClose} />
+    </>
   );
 }
---
+++ components/CourseDrawer/index.tsx
@@ -0,0 +1,70 @@
+import {
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  DrawerHeader,
+  DrawerBody,
+  Heading,
+  DrawerFooter,
+  Stack,
+  Button,
+} from "@chakra-ui/react";
+import VideoItem from "../VideoItem";
+import { Course } from "../../src/mockAPI/types";
+
+type CourseDrawerProps = {
+  isOpen: boolean;
+  course?: Course;
+  onClose: () => void;
+};
+
+export default function CourseDrawer({
+  isOpen,
+  onClose,
+  course,
+}: CourseDrawerProps) {
+  return (
+    <Drawer onClose={onClose} isOpen={isOpen} size={{ lg: "lg", base: "full" }}>
+      <DrawerOverlay />
+      <DrawerContent color="purple">
+        <DrawerCloseButton />
+        <DrawerHeader fontSize="xx-large" as="h2">
+          {course?.title}
+        </DrawerHeader>
+        <DrawerBody>
+          <Heading size="xx-large" as="h3">
+            Descrição
+          </Heading>
+          <p>{course?.description}</p>
+          <Heading size="xx-large" mt={6} as="h3">
+            Vídeos
+          </Heading>
+          {course?.courseVideos.map((video, index) => (
+            <VideoItem video={video} key={video.id} index={index} />
+          ))}
+        </DrawerBody>
+        <DrawerFooter>
+          <Stack width="full">
+            {/* <Button colorScheme="purple" onClick={() => console.log("Create Course")}>
+            Create Course
+          </Button> */}
+            <Button
+              width="100%"
+              colorScheme="purple"
+              onClick={() => console.log("Edit Course")}
+            >
+              Edit Course
+            </Button>
+            <Button
+              colorScheme="purple"
+              onClick={() => console.log("Delete Course")}
+            >
+              Delete Course
+            </Button>
+          </Stack>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  );
+}

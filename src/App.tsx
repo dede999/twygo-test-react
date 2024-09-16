817dbf0626c43178647642448773782e515bb1bb
Andre Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2024-09-16 18:10:33 -0300

    Refactor App component to seed courses if none exist

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import "./App.css";
 import CoursesList from "../components/CoursesList";
 import { Box, ChakraProvider } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useCourseStore } from "./state/courses";
 
 function App() {
+  const { courses, seedCourses } = useCourseStore((state) => state);
+
+  useEffect(() => {
+    console.log(courses);
+    if (courses.length == 0) {
+      seedCourses();
+    }
+  });
+
   return (
     <>
       <ChakraProvider>

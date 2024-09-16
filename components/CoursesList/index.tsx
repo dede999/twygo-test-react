97633fae494da126f470c2b102afd88fb94bf223
Andre Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2024-09-16 18:11:00 -0300

    Fetch courses by state manager

--- components/CoursesList/index.tsx
+++ components/CoursesList/index.tsx
@@ -1,8 +1,10 @@
 import { SimpleGrid } from "@chakra-ui/react";
 import CourseDisplay from "../CourseDisplay";
-import { courses } from "../../src/mockAPI";
+import { useCourseStore } from "../../src/state/courses";
 
 export default function CoursesList() {
+  const courses = useCourseStore((state) => state.courses);
+
   return (
     <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={4}>
       {courses.map((course) => (
